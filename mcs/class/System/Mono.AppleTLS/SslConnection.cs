1e176c072d9c21e1b74e96608d40749d96ede9a9
Chris Hamons <chris.hamons@xamarin.com>
2017-01-25 17:25:14 -0500

    Remove delegates

---
+++ mcs/class/System/Mono.AppleTLS/SslConnection.cs
@@ -0,0 +1,22 @@
+//
+// SslConnection
+//
+// Authors:
+//	Sebastien Pouliot  <sebastien@xamarin.com>
+//
+// Copyright 2014 Xamarin Inc.
+//
+
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+using ObjCRuntime;
+
+namespace Security {
+
+	delegate SslStatus SslReadFunc (IntPtr connection, IntPtr data, /* size_t* */ ref IntPtr dataLength);
+
+	delegate SslStatus SslWriteFunc (IntPtr connection, IntPtr data, /* size_t* */ ref IntPtr dataLength);
+}
--- mcs/class/System/Mono.AppleTLS/Stubs.cs
+++ mcs/class/System/Mono.AppleTLS/Stubs.cs
@@ -6,10 +6,6 @@ using nint = System.IntPtr;
 
 namespace Security
 {
-	delegate SslStatus SslReadFunc (IntPtr connection, IntPtr data, /* size_t* */ ref nint dataLength);
-
-	delegate SslStatus SslWriteFunc (IntPtr connection, IntPtr data, /* size_t* */ ref nint dataLength);
-	
 	internal class SecKeyChain {
 		internal static SecIdentity FindIdentity (SecCertificate certificate, bool throwOnError = false) { return null; }
 	}
